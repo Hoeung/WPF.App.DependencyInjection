7f34e275ad6e8ed132b9f80a7e3d3bbbf272f3c8
Hoeung <hodolhodol@naver.com>
2024-12-30 21:23:01 +0900

    WPF 프로젝트 초기 설정 및 주요 파일 추가
    .gitignore 파일에 Visual Studio 관련 항목 추가.
    AssemblyInfo.cs에 WPF 애플리케이션 테마 정보 설정 코드 추가.
    CustomControl1.cs에 WPF 커스텀 컨트롤 정의 코드 추가.
    DependencyInjection.Main.csproj 파일 추가.
    Generic.xaml에 CustomControl1 스타일 정의 리소스 딕셔너리 추가.
    UserControl1.xaml 및 UserControl1.xaml.cs에 새로운 UserControl 정의 및 코드 비하인드 추가.
    App.xaml에 애플리케이션 리소스와 Material Design 테마 설정 추가.
    App.xaml.cs에 애플리케이션 시작 시 MainWindow 표시 코드 추가.
    MainWindow.xaml 및 MainWindow.xaml.cs에 MainWindow 정의 및 코드 비하인드 추가.
    WPF.App.DependencyInjection.csproj 및 WPF.App.DependencyInjection.sln 파일 추가.
    AppBootstrapper.cs에 MVVM 패턴 지원 부트스트래퍼 클래스 추가.
    ViewModelBase.cs에 모든 뷰모델의 기본 클래스 추가.
    WPF.Core.csproj 파일 추가.

---
+++ WPF.Core/AppBootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WPF.Core;
+
+public abstract class AppBootstrapper
+{
+    /// <summary>
+    /// 뷰모델을 서비스 컬렉션에 등록합니다.
+    /// </summary>
+    /// <param name="services">서비스 컬렉션</param>
+    protected abstract void RegisterViewModels(IServiceCollection services);
+
+    /// <summary>
+    /// 뷰를 서비스 컬렉션에 등록합니다.
+    /// </summary>
+    /// <param name="services">서비스 컬렉션</param>
+    protected abstract void RegisterViews(IServiceCollection services);
+
+    /// <summary>
+    /// 애플리케이션 시작 시 호출되는 메서드입니다.
+    /// </summary>
+    protected abstract void OnStartup();
+
+    /// <summary>
+    /// 애플리케이션을 실행합니다.
+    /// </summary>
+    public void Run()
+    {
+        var services = new ServiceCollection();
+        RegisterViewModels(services);
+        RegisterViews(services);
+
+        Ioc.Default.ConfigureServices(services.BuildServiceProvider());
+
+        OnStartup();
+    }
+}
---
+++ WPF.Core/ViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WPF.Core;
+
+/// <summary>
+/// 모든 뷰모델의 기본 클래스입니다.
+/// </summary>
+public class ViewModelBase : ObservableObject
+{
+}
